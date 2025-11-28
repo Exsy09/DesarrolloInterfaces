d53c56b3e7d5453070b57a3f3f734b459bde406f
Aitor Coscollano <c.v.s.aitor@gmail.com>
2025-11-28 10:20:00 +0100

    objetos kevin

---
+++ Javascript/HTML/04-ejemplo.js
@@ -0,0 +1,8 @@
+// Ejercicio 1. Función que recibe una base y un exponente y devuelve base ** exponente.
+// Mostrar en consola el resultado 2 elevado a 3 y 5 elevado a 2
+function potencia(base, exponente){
+    return base ** exponente;
+}
+
+console.log("2 elevado a 3 es", potencia(2,3));
+console.log("5 elevado a 2 es", potencia(5,2));
